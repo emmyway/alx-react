a01d67e5692d4a08af7a8102161de9887f540279
emmanuel nnaji <realreach@yahoo.com>
2024-08-26 02:56:02 +0100

    rendered a hard coded notification

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <Notificatiins />
+  </React.StrictMode>,
+  document.getElementById('root-notifications')
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
