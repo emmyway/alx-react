fb41c4e16c7c9b0bcb489d959b3887144b41fb2a
Emmanuel Nnaji <realreaach@yahoo.com>
2024-09-14 18:49:44 +0100

    debugged files

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,18 +1,9 @@
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  // Start from the given object
-  let result = object;
+  // Convert the plain JavaScript object to an Immutable Map
+  const immutableObject = fromJS(object);
 
-  // Iterate over each key in the array to access the value at the defined path
-  for (let i = 0; i < array.length; i += 1) {
-    // If the key exists, update result to the corresponding value
-    if (result[array[i]] !== undefined) {
-      result = result[array[i]];
-    } else {
-      // If any key in the path doesn't exist, return undefined
-      return undefined;
-    }
-  }
-
-  // Return the final value, which could be a string, object, or Map
-  return result;
+  // Use the getIn method to access the value at the specified path
+  return immutableObject.getIn(array);
 }
