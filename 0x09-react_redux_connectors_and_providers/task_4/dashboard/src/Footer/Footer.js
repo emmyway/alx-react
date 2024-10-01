a3b8911222b58d1dfda27cd6753acddeb20156c2
Emmanuel Nnaji <realreaach@yahoo.com>
2024-10-01 13:30:59 +0100

    added solution to task 4

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import './Footer.css';
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
+import { connect } from 'react-redux';
+
+function Footer () {
+    // const { user } = useContext(AppContext);
+    const user = props.user;
+    return (
+        <>
+        <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </div>
+        </>
+    );
+}
+
+export function mapStateToProps(state) {
+  return {
+    user: state.get('user')
+  };
+}
+
+export default connect(mapStateToProps)(Footer);
